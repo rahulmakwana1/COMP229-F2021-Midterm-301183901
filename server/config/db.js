02b96c25423f69b6551ba69ae24ea820bb409d89
rahulmakwana1 <rahulmakwana3535@gmail.com>
2021-10-23 16:22:28 -0400

    mid term exam

---
+++ server/config/db.js
@@ -0,0 +1,7 @@
+
+module.exports =
+{
+    //"URI" : "mongodb://localhost/contact_list"
+    "URI": "mongodb+srv://rahul:AEotqXglzM6QTkcM@cluster0.sac9w.mongodb.net/booklist?retryWrites=true&w=majority",
+    
+}
